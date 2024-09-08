8cab9816ab78a0a0617e2d82bd6bcbb0b621a62b
abdellah <abdellahedaoudi80@gmail.com>
2024-09-08 04:25:07 +0100

    Authorization

--- app/Context/MyContext.js
+++ app/Context/MyContext.js
@@ -8,15 +8,14 @@ export const MyContext = createContext();
 
 export const MyProvider = ({ children }) => {
   const [userDetails, setUserDetails] = useState([]);
-  const [userLinks, setUserLinks] = useState([]);
   const [messages, setMessages] = useState([]);
   const { user } = useUser();
   const EmailUser = user?.emailAddresses[0].emailAddress;
   const [previousNotificationCount, setPreviousNotificationCount] = useState(0);
 
   // const CLIENT_URL = "http://localhost:3000";
-  // const SERVER_URL = "http://localhost:9999" ;
-  // const SERVER_URL_V = "http://localhost:9999" ;
+  // const SERVER_URL = "http://localhost:2222" ;
+  // const SERVER_URL_V = "http://localhost:2222" ;
   const CLIENT_URL = process.env.NEXT_PUBLIC_CLIENT_URL;
   const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
   const SERVER_URL_V = process.env.NEXT_PUBLIC_SERVER_URL_V;
@@ -31,14 +30,10 @@ export const MyProvider = ({ children }) => {
   const audioRef = useRef(null);
 
   useEffect(() => {
-    // Connect to Socket.io
     socket.connect();
-
-    // Listen for incoming messages
     socket.on('receiveMessage', (data) => {
       setMessages((prevMessages) => [data, ...prevMessages]);
     });
-
     socket.on('receiveUpdatedMessage', (data) => {
       setMessages((prevMessages) =>
         prevMessages.map((msg) =>
@@ -46,14 +41,11 @@ export const MyProvider = ({ children }) => {
         )
       );
     });
-
     socket.on('receiveDeletedMessage', (id) => {
       setMessages((prevMessages) =>
         prevMessages.filter((msg) => msg._id !== id)
       );
     });
-
-    // Clean up on unmount
     return () => {
       socket.disconnect();
     };
@@ -91,21 +83,7 @@ export const MyProvider = ({ children }) => {
 
     getMessages();
   }, [SERVER_URL_V]);
-  // links
-  useEffect(() => {
-    axios
-      .get(`${SERVER_URL_V}/links`, {
-        headers: {
-          'Authorization': `Bearer ${process.env.NEXT_PUBLIC_TOKEN}` // Include the token in the Authorization header
-        }
-      })
-      .then((res) => {
-        setUserLinks(res.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching user details:", error);
-      });
-  }, [SERVER_URL_V]);
+
 
   const Notification = messages.filter(
     (fl) => fl.to === EmailUser && fl.from !== EmailUser
@@ -125,8 +103,6 @@ export const MyProvider = ({ children }) => {
         CLIENT_URL,
         SERVER_URL,
         userDetails,
-        userLinks,
-        setUserLinks,
         EmailUser,
         Notification,
         SERVER_URL_V,
